8fb4f74875b7afa9c2432b397a3f00244250cae2
Cix8 <francesco.natal_fn@libero.it>
2022-04-19 13:49:46 +0200

    add jsnack4 program

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,1 +1,10 @@
-alert('ciao');
+const userNumber = prompt('Inserisci un numero di 4 cifre');
+let somma = 0;
+
+for (let i = 0; i < 4; i++) {
+    const singleNumber = parseInt(userNumber.charAt(i));
+    console.log(singleNumber);
+    somma += singleNumber;
+}
+
+console.log('Il numero di 4 cifre inserito è:', userNumber,'La somma delle 4 cifre è uguale a:',somma);
