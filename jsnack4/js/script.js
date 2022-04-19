fa109500407cc150b9d929ba559689b5815e924e
Cix8 <francesco.natal_fn@libero.it>
2022-04-19 14:35:19 +0200

    'the program is getting smarter'

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,10 +1,13 @@
 const userNumber = prompt('Inserisci un numero di 4 cifre');
 let somma = 0;
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < userNumber.length; i++) {
     const singleNumber = parseInt(userNumber.charAt(i));
     console.log(singleNumber);
     somma += singleNumber;
 }
-
-console.log('Il numero di 4 cifre inserito è:', userNumber,'La somma delle 4 cifre è uguale a:',somma);
+if (userNumber.length === 4) {
+    console.log('Il numero inserito è:', userNumber,'La somma delle singole cifre è uguale a:',somma);
+} else {
+    console.log('Ti avevo chiesto di scegliere un numero di 4 cifre ma ti mostro comunque il risultato, il numero inserito è:', userNumber, 'La somma delle singole cifre è:', somma);
+}
